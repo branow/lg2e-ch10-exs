9d47cd2cfc2b8344c01920d4ff178494a613bc6e
branow <bodnarorestdraw@gmail.com>
2024-08-24 20:27:20 +0300

    implement generic use

--- add/add.go
+++ add/add.go
@@ -2,12 +2,19 @@
 // an operation of addition for two values.
 package add
 
+import "golang.org/x/exp/constraints"
+
 // Add adds the two given values and return the value of
 // their sum.
 //
 // More information on addition can be found at [addition]
 //
 // [addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
+}
+
+// Integer is a constraint that permits any integer or float type.
+type Number interface {
+	constraints.Integer | constraints.Float
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module lg2e-ch10-exs
 
 go 1.23.0
+
+require golang.org/x/exp v0.0.0-20240823005443-9b4947da3948 // indirect
