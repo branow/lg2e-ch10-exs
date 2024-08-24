8f735c3849436e4c973a83a848201d8647feb332
branow <bodnarorestdraw@gmail.com>
2024-08-24 20:21:11 +0300

    add godoc comments

--- add/add.go
+++ add/add.go
@@ -1,5 +1,13 @@
+// Package add provides the method that helps perform
+// an operation of addition for two values.
 package add
 
+// Add adds the two given values and return the value of
+// their sum.
+//
+// More information on addition can be found at [addition]
+//
+// [addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
